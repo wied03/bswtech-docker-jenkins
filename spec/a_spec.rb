6d9e4b3e4bd8c52731633a883e65b7de5cb07673
Brady Wied <brady@bswtechconsulting.com>
2016-08-03 12:02:19 -0600

    see if jenkins triggers

--- spec/a_spec.rb
+++ spec/a_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Jenkins image' do
+describe 'Jenkins image test' do
   # Could use sleep here, but maybe it's better to use the retryable GEM
   describe command('curl -I http://localhost:8080') do
     its(:exit_status) { is_expected.to eq 0 }
