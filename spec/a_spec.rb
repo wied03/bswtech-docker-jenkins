88e7206a172c205fd46f952ee6318c3ae21060de
Brady Wied <brady@bswtechconsulting.com>
2016-08-03 13:05:19 -0600

    add helpers/other things

--- spec/a_spec.rb
+++ spec/a_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
+require 'retryable'
 
-describe 'Jenkins image test' do
-  # Could use sleep here, but maybe it's better to use the retryable GEM
-  describe command('curl -I http://localhost:8080') do
-    its(:exit_status) { is_expected.to eq 0 }
+describe 'Jenkins container' do
+  xit 'listens on port 8080' do
+    Retryable.retryable(tries: 10,
+                        on: RSpec::Expectations::ExpectationNotMetError,
+                        sleep: 5) do
+      cmd = command('curl http://localhost:8080')
+      expect(cmd.exit_status).to eq 0
+      expect(cmd.stdout).to include '<html>'
+    end
   end
 
-  pending 'volumes, run as user, permissions'
+  describe 'docker logs' do
+    pending
+  end
+
+  it 'shows logs without errors' do
+    pending "#{current_container_id}"
+  end
+
+  describe docker_container do
+    it { is_expected.to exist }
+    it { is_expected.to be_running }
+    it { is_expected.to have_volume '/var/jenkins_home', JENKINS_VOLUME }
+  end
+
+  pending 'JIRA'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'serverspec'
+require 'serverspec_patch'
 
 set :backend, :docker
 set :docker_image, ENV['IMAGE_TAG']
@@ -13,8 +14,15 @@ set :docker_container_create_options, {
   }
 }
 
+module DockerSpecHelper
+  def current_container_id
+    Specinfra.backend.instance_variable_get(:@container).id
+  end
+end
+
 RSpec.configure do |config|
+  config.include DockerSpecHelper
   config.before(:suite) do
-    FileUtils.rm_rf JENKINS_VOLUME
+    #FileUtils.rm_rf JENKINS_VOLUME
   end
 end
