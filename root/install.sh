655ba5369266dd423e85194d7ebee95144e2ba1f
Brady Wied <brady@bswtechconsulting.com>
2016-08-05 12:58:00 -0600

    Permissions, also be more explicit

--- root/install.sh
+++ root/install.sh
@@ -16,7 +16,9 @@ echo "groupadd -g ${JENKINS_GID} ${JENKINS_GROUP}"
 echo "useradd -r -d ${JENKINS_HOME} -u ${JENKINS_UID} -g ${JENKINS_GID} -m -s /bin/bash ${JENKINS_USER}"
 
 # Create Container
-chroot ${HOST} docker create --name ${NAME} --cap-drop=all --read-only --tmpfs /run --tmpfs /tmp:exec -v ${JENKINS_HOME}:/var/jenkins_home:Z ${ARGS[@]:1} ${IMAGE}
+REST_ARGS=${ARGS[@]:1}
+echo "Mounting Jenkins home from ${JENKINS_HOME}. Adding rest of docker args as well: ${REST_ARGS}"
+chroot ${HOST} docker create --name ${NAME} --cap-drop=all --read-only --tmpfs /run --tmpfs /tmp:exec -v ${JENKINS_HOME}:/var/jenkins_home:Z ${REST_ARGS} ${IMAGE}
 
 # Install systemd unit file for running container
 cp /jenkins_template.service > ${HOST}/etc/systemd/system/${NAME}.service
