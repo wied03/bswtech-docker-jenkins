e8d6d67e67c2d58f50eb89ea1acc5eadab68a1ed
Brady Wied <brady@bswtechconsulting.com>
2016-08-05 13:58:51 -0600

    fix cp issue, clarify logging

--- root/install.sh
+++ root/install.sh
@@ -11,17 +11,18 @@ fi
 JENKINS_HOME=$1
 ARGS=("$@")
 
+REST_ARGS=${ARGS[@]:1}
+echo "Creating container using Jenkins home from ${JENKINS_HOME}. Adding rest of docker args as well: ${REST_ARGS}"
+chroot ${HOST} docker create --name ${NAME} --cap-drop=all --read-only --tmpfs /run --tmpfs /tmp:exec -v ${JENKINS_HOME}:/var/jenkins_home:Z ${REST_ARGS} ${IMAGE}
+
+SYSTEMD_FILE=/etc/systemd/system/${NAME}.service
+echo "Setting up systemd file in ${SYSTEMD_FILE}"
+cp /jenkins_template.service ${HOST}${SYSTEMD_FILE}
+
+echo "Enabling systemd service"
+chroot ${HOST} systemctl enable ${SYSTEMD_FILE}
+
+echo 'Installation complete'
 echo 'Run the following commands on the host before starting the container:'
 echo "groupadd -g ${JENKINS_GID} ${JENKINS_GROUP}"
 echo "useradd -r -d ${JENKINS_HOME} -u ${JENKINS_UID} -g ${JENKINS_GID} -m -s /bin/bash ${JENKINS_USER}"
-
-# Create Container
-REST_ARGS=${ARGS[@]:1}
-echo "Mounting Jenkins home from ${JENKINS_HOME}. Adding rest of docker args as well: ${REST_ARGS}"
-chroot ${HOST} docker create --name ${NAME} --cap-drop=all --read-only --tmpfs /run --tmpfs /tmp:exec -v ${JENKINS_HOME}:/var/jenkins_home:Z ${REST_ARGS} ${IMAGE}
-
-# Install systemd unit file for running container
-cp /jenkins_template.service > ${HOST}/etc/systemd/system/${NAME}.service
-
-# Enabled systemd unit file
-chroot ${HOST} systemctl enable /etc/systemd/system/${NAME}.service
--- root/uninstall.sh
+++ root/uninstall.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-chroot ${HOST} systemctl disable /etc/systemd/system/${NAME}.service
-rm -f ${HOST}/etc/systemd/system/${NAME}.service
+SYSTEMD_FILE=/etc/systemd/system/${NAME}.service
+echo "Disabling systemd service ${SYSTEMD_FILE}"
+chroot ${HOST} systemctl disable ${SYSTEMD_FILE}
+echo 'Removing systemd service'
+rm -f ${HOST}${SYSTEMD_FILE}
 
+echo 'Uninstall complete'
 echo 'Run the following commands on the host to remove user/data'
 echo "userdel ${JENKINS_USER}"
