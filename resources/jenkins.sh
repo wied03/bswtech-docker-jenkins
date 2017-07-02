974433570752d231c95479de1c77b3de3efa7ede
Brady Wied <brady@wied.us>
2017-07-01 21:38:16 -0600

    Follow Jenkins recommendations

--- resources/jenkins.sh
+++ resources/jenkins.sh
@@ -28,7 +28,7 @@ find "${JENKINS_REF_DIR}/" -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  eval "JAVA_OPTS=\"${JAVA_OPTS} -DJENKINS_HOME=${JENKINS_HOME} -Dhudson.PluginManager.className=com.bswtechconsulting.jenkins.ReadOnlyPluginManager\" exec /usr/libexec/tomcat/server start"
+  eval "JAVA_OPTS=\"${JAVA_OPTS} -DJENKINS_HOME=${JENKINS_HOME} -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dhudson.PluginManager.className=com.bswtechconsulting.jenkins.ReadOnlyPluginManager\" exec /usr/libexec/tomcat/server start"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
