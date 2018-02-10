09f37c0b1092b76f08c56dc706120b7f5ef96ee8
Brady Wied <brady@bswtechconsulting.com>
2018-02-10 11:22:12 -0700

    First manifest parse working

--- lib/bsw_tech/jenkins_gem/manifest_parser.rb
+++ lib/bsw_tech/jenkins_gem/manifest_parser.rb
@@ -1,11 +1,19 @@
 module BswTech
   module JenkinsGem
     class ManifestParser
-      def initialize(manifest_contents)
-      end
+      attr_reader :gem_spec
+      SEPARATOR = ': '
 
-      def gem_spec
-        nil
+      def initialize(manifest_contents)
+        properties = Hash[manifest_contents
+                            .split("\n")
+                            .map do |property_line|
+          parts = property_line.split SEPARATOR
+          [parts[0], parts[1..-1].join(SEPARATOR)]
+        end]
+        @gem_spec = Gem::Specification.new do |s|
+          s.description = properties['Specification-Title']
+        end
       end
     end
   end
