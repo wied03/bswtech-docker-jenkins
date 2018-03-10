324644cc3fe9e90da7de89b5ba9ba71369a02ccb
Brady Wied <brady@bswtechconsulting.com>
2018-03-10 12:45:09 -0700

    Got all tests to pass

--- resources/jenkins.sh
+++ resources/jenkins.sh
@@ -11,7 +11,8 @@ mkdir -p /var/cache/tomcat/temp
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  eval "JAVA_OPTS=\"${JAVA_OPTS} -DJENKINS_HOME=${JENKINS_HOME} -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dhudson.PluginManager.className=com.bswtechconsulting.jenkins.ReadOnlyPluginManager\" exec /usr/libexec/tomcat/server start"
+  # user.home needs to be explicitly set because we don't have an actual user account, just a UID a and Ivy depends on this
+  eval "JAVA_OPTS=\"${JAVA_OPTS} -DJENKINS_HOME=${JENKINS_HOME} -Duser.home=${JENKINS_HOME} -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dhudson.PluginManager.className=com.bswtechconsulting.jenkins.ReadOnlyPluginManager\" exec /usr/libexec/tomcat/server start"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
